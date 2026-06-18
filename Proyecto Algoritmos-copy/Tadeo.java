509f067a358a2cbfeae6aec42ff052188534c345
TadeoCarabajal <tadeocarabajal1@gmail.com>
2026-06-18 20:54:22 -0300

    Eliminación del método "exploit"
    Este método, perteneciente a la clase Tadeo, rompe el programa al presentar un while true.

--- Proyecto Algoritmos-copy/Tadeo.java
+++ Proyecto Algoritmos-copy/Tadeo.java
@@ -4,9 +4,4 @@ public class Tadeo{
     public Tadeo(){
         
     }
-    public void exploit(){
-        while(true){
-            System.out.println("XD, SOS UN RE PELOTUDO");
-        }
-    }
 }
