af12a83d2952b117039e9aafd17305ef0a7a1ae8
Alexis <alexisgamerrprro9567@gmail.com>
2026-06-19 12:51:28 -0300

    .

---
+++ Proyecto Algoritmos-copy/KGramas.java
@@ -0,0 +1,11 @@
+import java.util.List; 
+import java.util.Map;
+
+public class KGramas{
+    private Map<List<String>, Map<String, Integer>> mapaTrans;
+    
+    public KGramas(){
+        
+    }
+    
+}
